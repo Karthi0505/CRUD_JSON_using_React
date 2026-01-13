1a7969dee02e8b947d2d7eadb099b4d69e97c209
karthi0505 <karthi.uideveloper@gmail.com>
2026-01-13 08:56:42 +0530

    Create

---
+++ src/All_in_one.jsx
@@ -0,0 +1,92 @@
+import React, { useState, useEffect } from 'react'
+
+function All_in_one() {
+    const [title, setTitle] = useState("");
+    const [author, setAuthor] = useState("");
+
+    const [posts, setPosts] = useState([]);
+    
+    const [isEditing, setIsEditing] = useState(false); //for update only
+
+
+    // READ -----------------------------------------
+    useEffect(() => {
+        fetch("http://localhost:3000/posts")
+            .then(res => res.json())
+            .then(data => setPosts(data));
+    }, []);
+
+
+    // CREATE ---------------------------------------
+    const addPost = (post) => {
+        setPosts(prev => [...prev, post]);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const newPost = { title, author };
+
+        //SAVE THE NEW VALUE IN OUR DUMMY JSON API
+        fetch("http://localhost:3000/posts", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(newPost),
+        }) // AFTER SAVING IN JSON - GET THE LATEST DATA FROM THE JSON - AND SAVE THE LATEST DATA IN STATE.
+            .then(res => res.json())
+            .then(data => {
+                //Update state
+                setPosts(prev => [...prev, data]); //ONCE STATE VALUE UPDATES - COMPONENT RE-RENDERS
+
+                //Make the inputs Empty
+                setTitle("");
+                setAuthor("");
+            });
+    };
+
+
+    // UPDATE -----------------------------
+    const updatePost = (updatedPost) => {
+        setPosts(prev =>
+            prev.map(post =>
+                post.id === updatedPost.id ? updatedPost : post
+            )
+        );
+    };
+
+
+
+    console.log(posts)
+
+    return (
+        <>
+            <h1>All CRUD operation in same component</h1>
+
+            {/* Read */}
+            <ul>
+                {posts.map(post => (
+                    <li key={post.id}>
+                        <strong>{post.title}</strong> - {post.author}
+                    </li>
+                ))}
+            </ul>
+
+            {/* Create */}
+            <form onSubmit={handleSubmit}>
+                <input
+                    placeholder="Title"
+                    value={title}
+                    onChange={e => setTitle(e.target.value)}
+                />
+                <input
+                    placeholder="Author"
+                    value={author}
+                    onChange={e => setAuthor(e.target.value)}
+                />
+                <button>Add</button>
+            </form>
+        </>
+    )
+}
+
+export default All_in_one
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+// import App from './App.jsx'
+import All_in_one from './All_in_one.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <All_in_one />
   </StrictMode>,
 )
